fb02436fb307bc4a89f29ceb99cf0f85d03dde53
cybernetck <mattswomack@gmail.com>
2018-04-05 08:47:24 -0500

    removed counter from product card and move to checkout page

--- client/src/components/Product/ProductContainer.js
+++ client/src/components/Product/ProductContainer.js
@@ -10,12 +10,12 @@ class ProductContainer extends Component {
             }
     }
 
-    // getImage = () => {
-    //     var fileId = this.state.products.relationships.main_image.data.id;
-    //     var file   = this.state.imageIdArr.find( img => fileId === img.id);
-    //     console.log ("file and fileid inside getImage", fileId, file);
-    //     return file;
-    // }
+    getImage = () => {
+        // var fileId = this.state.products.relationships.main_image.data.id;
+        // var file   = this.state.imageIdArr.find( img => fileId === img.id);
+    
+        console.log("inside getImage");
+    }
 
     // componentDidMount() {
     //  console.log("images", props.imageIdArr);
